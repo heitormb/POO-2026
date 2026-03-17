836f821952da8dd0e4e707fb95eaff1c667a6732
heitormb <heitormb2018@gmail.com>
2026-03-17 18:56:29 +0000

    aula2 dever feito

--- aula02/ex01.py
+++ aula02/ex01.py
@@ -3,3 +3,15 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
+sp = 0
+si = 0
+if a % 2 == 0: sp += a 
+else: si += a
+if b % 2 == 0: sp += a 
+else: si += b
+if c % 2 == 0: sp += a 
+else: si += c
+if d % 2 == 0: sp += a 
+else: si += d
+print("Soma dos pares:", sp)
+print("Soma dos ímpares:", si)
