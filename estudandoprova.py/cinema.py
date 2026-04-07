ef824f010fa34a8a934351192d601d3274b4a955
heitormb <heitormb2018@gmail.com>
2026-04-07 07:31:02 +0000

    estudo

---
+++ estudandoprova.py/cinema.py
@@ -0,0 +1,32 @@
+class Cinema():
+    def __init__(self, dia, horario):
+        self.dia = dia
+        self.horario = horario
+
+    def calcular_valor(self, valor):
+        if self.dia == "segunda":
+            valor = 16
+        elif self.dia == "terça":
+            valor = 16
+        elif self.dia == "quinta":
+            valor = 16
+        elif self.dia == "quarta":
+            valor = 8
+        elif self.dia == "sexta":
+            valor = 20
+        elif self.dia == "sábado":
+            valor = 20
+        elif self.dia == "domingo":
+            valor = 20
+
+        if self.horario in range(17, 25):
+            valor = valor + valor/2
+
+        return valor
+
+
+dia = input("digite o dia do filme: ")
+horario = input("digite o horário do filme: ")
+
+filme = Cinema(dia, horario)
+print
