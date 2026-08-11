ef616d1ceec150363dd862f7d08874eef7fa432f
heitormb <heitormb2018@gmail.com>
2026-08-11 18:53:51 +0000

    aula_11.08

---
+++ aula_11.08/index.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+
+RetanguloUI.main()
+
+#st.header("POO em Python com Stremlit")
+#if st.button("Clique Aqui"):
+#    st.write("Bem-vindo(a) ao Stremlit")
